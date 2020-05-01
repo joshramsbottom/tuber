20e1740e1114a36fb935e4f77538c502a19c0700
Joshua Ramsbottom <joshuaramsbottom@electrum.co.za>
2020-05-01 16:25:37 +0200

    Initial Commit

---
+++ tuber/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tuber
+//
+//  Created by Joshua Ramsbottom on 2020/05/01.
+//  Copyright © 2020 jramsbottom. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tuber/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tuber
+//
+//  Created by Joshua Ramsbottom on 2020/05/01.
+//  Copyright © 2020 jramsbottom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
